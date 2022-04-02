4d345daf5ed9c4de86163ac5014859a2fa0322c1
Stephan <stephan.semerad@gmail.com>
2022-04-03 01:37:44 +0200

    revision

--- pln.py
+++ pln.py
@@ -1,41 +1,39 @@
-import xmltodict
-from urllib.request import urlopen
-from decimal import Decimal
-from time import gmtime, strftime
+import requests
+from datetime import datetime
 
-today = strftime("%Y-%m-%d", gmtime())
-list =[]
-years = ['2018']
-for year in years:
-    for number in range(1000):
-        if len(str(number)) == 1:
-            number = '00'+ str(number)
-        elif len(str(number)) == 2:
-            number = '0'+ str(number)
+class plnFX:
+    def __init__(self):
+        self.year = None
+        self.url = 'https://api.nbp.pl/api/exchangerates/rates/A/EUR'
+
+    def get_year(self, year):
+        today       = datetime.now()
+        start_year  = datetime(int(year), 1, 1)
+        end_year    = datetime(int(year), 12, 31)
+        end_year    = today if end_year > today else end_year
+        url = f'{self.url}/{start_year.date()}/{end_year.date()}/'
+        response = requests.get(url)       
+        if not response.status_code == 200:
+            print('Error could not retrieve from API, response status code: {response.status_code}')
+            return []
         else:
-            number = str(number)
-        print (str(number))
-        url = 'https://www.nbp.pl/kursy/xml/en/'+year[2:4]+'a'+str(number)+'en.xml'
-        print(url)
-        try:
-            file = urlopen(url)
-            data = xmltodict.parse(file.read())
-            data = data['exchange_rates']
-
-            date = data['@date']
-            for i in data['mid-rate']:
-                if i['@code'] == 'EUR':
-                    rate = i['#text']
-
-            print('today: '+str(today))
-            print('date: '+str(date))
-            print('rate: '+str(rate))
-            list.append([date, rate])
-            if date == today:
-                break
-        except:
-            continue
-
-print('PLN rates')
-for i in list:
-    print(i)
+            data = response.json()
+            data = data.get('rates', [])
+            data = [{'date' :x['effectiveDate'], 'rate': x['mid']} for x in data]
+            return data
+        
+    def get_date(self, date):
+        date = datetime.strptime(date, "%Y-%m-%d")
+        data = self.get_year(date.year)
+        for x in data: 
+            if x['date'] == str(date.date()): 
+                return x; break
+        return None
+       
+if __name__ == '__main__':
+    fx = plnFX()
+    year_rate = fx.get_year('2022')
+    print('year_rate: ', year_rate)
+    print()
+    day_rate = fx.get_date('2022-01-03')
+    print('day_rate: ', day_rate)
